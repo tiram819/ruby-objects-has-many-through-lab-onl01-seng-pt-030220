ea631ab4ae4f0ee02d2eb0288b0dd2bd73ed79ff
Tabassum Hassan <tiram819@gmail.com>
2020-04-03 01:00:43 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   Appointment.new(date,patient,self)
  end
   
-  def appointment
+  def appointments
     Appointment.all.select do |appointment|
       appointment.doctor = self
   end
