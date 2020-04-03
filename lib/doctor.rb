d99fedea38e86f05114d86b15069b20a080b9e1e
Tabassum Hassan <tiram819@gmail.com>
2020-04-03 00:58:18 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  @@all=[]
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(patient,date)
+  Appointment.new(date,patient,self)
+ end
+  
+  def appointment
+    @appointments
+  end
+  
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
+
